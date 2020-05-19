7c85c0accb50647bd83b07b370bc9df49636bb10
Nik <nerdvestor@gmail.com>
2020-05-19 11:16:49 -0700

    done

---
+++ index.js
@@ -0,0 +1,53 @@
+const classifier = knnClassifier.create()
+const webcamElement = document.getElementById("webcam")
+
+let net
+
+async function app() {
+  console.log("Loading mobilnet...")
+
+  net = await mobilenet.load()
+
+  console.log("Loaded model")
+
+  const webcam = await tf.data.webcam(webcamElement)
+
+  const addExample = async (classId) => {
+    const img = await webcam.capture()
+
+    const activation = net.infer(img, true)
+
+    classifier.addExample(activation, classId)
+
+    img.dispose()
+  }
+
+  document.getElementById("dog").addEventListener("click", () => addExample(0))
+  document.getElementById("cat").addEventListener("click", () => addExample(1))
+  document
+    .getElementById("snake")
+    .addEventListener("click", () => addExample(2))
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture()
+
+      const activation = net.infer(img, "conv_preds")
+
+      const result = await classifier.predictClass(activation)
+
+      const classes = ["Dog", "Cat", "Snake"]
+
+      document.getElementById("console").innerText = `
+                prediction: ${classes[result.label]}\n
+                probabilty: ${result.confidences[result.label]}
+            `
+
+      img.dispose()
+    }
+
+    await tf.nextFrame()
+  }
+}
+
+app()
